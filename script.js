0c871b728dafa97cb6612925989d2e8d575c382c
SHIV PRAKASH SINGH <sshivprakash826@gmail.com>
2026-03-18 19:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Get dropdown
+const select = document.getElementById("colorSelect");
+
+// Get button
+const btn = document.querySelector('input[type="button"]');
+
+// Remove selected option
+btn.addEventListener("click", function () {
+  const selectedIndex = select.selectedIndex;
+
+  if (selectedIndex !== -1) {
+    select.remove(selectedIndex);
+  }
+});
